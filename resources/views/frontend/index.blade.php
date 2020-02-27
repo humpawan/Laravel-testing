4ea607177693f3b0098104a8230c2c4a3b7b72c5
pawanscaletech <hum2250@gmail.com>
2020-02-27 16:55:46 +0530

    -navbar page

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+    <title>Index-Page</title>
+    <link rel="stylesheet" href="{{ asset("../../public/css/style.css") }}">
+    <style>
+        body {
+            font-size: 28px;
+            font-style: oblique;
+            background-color: #7a7a7b;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            position: -webkit-sticky;
+            position: sticky;
+            top: 0;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: #4CAF50;
+        }
+    </style>
+</head>
+<body>
+<div class="header">
+    <h2>Laravel-Testing</h2>
+</div>
+<ul>
+    <li><a href="#">Chapter-1</a></li>
+    <li><a class="active" href="#">Chapter-2</a></li>
+    <li><a href="#">Chapter-3</a></li>
+</ul>
+<section>
+    <aside>
+        <h3>Title goes here</h3>
+    </aside>
+    <blockquote>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. In cursus turpis massa tincidunt dui ut ornare lectus sit. Tempus imperdiet nulla
+            malesuada pellentesque elit eget gravida cum. Nunc pulvinar sapien et ligula. Lorem ipsum dolor sit amet
+            consectetur adipiscing elit duis. Facilisis magna etiam tempor orci eu lobortis. Aenean vel elit scelerisque
+            mauris pellentesque pulvinar. Consectetur adipiscing elit ut aliquam purus. Tincidunt eget nullam non nisi
+            est sit amet. Est velit egestas dui id ornare arcu odio ut. Viverra nam libero justo laoreet sit amet cursus
+            sit amet. Placerat duis ultricies lacus sed turpis tincidunt id. Posuere sollicitudin aliquam ultrices
+            sagittis orci a. Vel eros donec ac odio tempor orci dapibus ultrices. Ultricies mi quis hendrerit dolor. Mi
+            sit amet mauris commodo quis imperdiet massa. Pharetra vel turpis nunc eget. Venenatis a condimentum vitae
+            sapien pellentesque habitant morbi tristique senectus. Diam quis enim lobortis scelerisque fermentum dui
+            faucibus. Integer vitae justo eget magna.
+            Semper risus in hendrerit gravida. Faucibus nisl tincidunt eget nullam non. Feugiat sed lectus vestibulum
+            mattis ullamcorper velit. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Pulvinar
+            pellentesque habitant morbi tristique. Diam maecenas ultricies mi eget mauris pharetra et ultrices neque.
+            Integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Aliquam etiam erat velit
+            scelerisque in dictum non. Rhoncus dolor purus non enim praesent elementum. Consectetur libero id faucibus
+            nisl tincidunt eget nullam non. Egestas purus viverra accumsan in. Eleifend donec pretium vulputate sapien
+            nec sagittis. Lacus luctus accumsan tortor posuere ac ut consequat semper viverra. Varius vel pharetra vel
+            turpis nunc eget lorem. Nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi.
+
+            Egestas dui id ornare arcu odio ut sem nulla. Ut tortor pretium viverra suspendisse potenti nullam ac. Sed
+            euismod nisi porta lorem mollis aliquam ut. Aenean et tortor at risus viverra adipiscing at in tellus.
+            Placerat duis ultricies lacus sed turpis tincidunt. Quisque egestas diam in arcu cursus. Vestibulum lorem
+            sed risus ultricies tristique nulla aliquet. Morbi tristique senectus et netus et. In pellentesque massa
+            placerat duis. Lectus sit amet est placerat in egestas. Egestas diam in arcu cursus. Egestas sed tempus urna
+            et pharetra pharetra massa. At auctor urna nunc id cursus metus aliquam. Eget duis at tellus at urna
+            condimentum mattis. Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Magna
+            fermentum iaculis eu non diam phasellus vestibulum. Sed elementum tempus egestas sed sed risus pretium.
+
+            Pulvinar pellentesque habitant morbi tristique senectus et netus. At lectus urna duis convallis convallis
+            tellus id. Vitae ultricies leo integer malesuada nunc vel. Nisl rhoncus mattis rhoncus urna neque. Auctor eu
+            augue ut lectus arcu bibendum at varius. Turpis massa tincidunt dui ut. Facilisis mauris sit amet massa.
+            Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Feugiat scelerisque varius morbi enim nunc
+            faucibus a. Accumsan tortor posuere ac ut consequat. Nisl nunc mi ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec.
+
+            Non tellus orci ac auctor augue mauris augue neque. Sit amet porttitor eget dolor morbi non. Placerat in
+            egestas erat imperdiet sed euismod nisi. Eu feugiat pretium nibh ipsum consequat nisl vel. At elementum eu
+            facilisis sed odio morbi quis commodo. Sed vulputate mi sit amet mauris. Cras semper auctor neque vitae
+            tempus. Nulla aliquet porttitor lacus luctus accumsan tortor posuere. Vel eros donec ac odio tempor orci. Mi
+            proin sed libero enim sed faucibus turpis in. Ullamcorper sit amet risus nullam eget felis. Vitae tortor
+            condimentum lacinia quis vel. Nulla facilisi etiam dignissim diam quis enim lobortis scelerisque.
+        </p>
+    </blockquote>
+</section>
+</body>
+</html>
